0b431d952954ef59958f9aab8119c40377109573
Igor Gazio <gaumgazineo@hotmail.com>
2025-04-12 03:12:33 -0300

    atualizando os arquivos script.js, style.css e adicionando colaborador.css

--- main/script.js
+++ main/script.js
@@ -3,61 +3,50 @@ const inputEl = document.getElementById("input-pesquisa");
 
 const colaboratorsInfo = [
   {
-    name: "Igor",
+    name: "Igor Gazineo",
     isMale: true,
     hasBackButton: true,
-    path: "/igor/igor.html",
+    path: "/igor-000/igor.html",
     picture:
       "https://media.licdn.com/dms/image/v2/D4D03AQEQsxrzUhbVkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705305900860?e=1749686400&v=beta&t=rhNEWW401j4BCNHxM5ir0y7l_vE9UC3qnPYY1yIZjdY",
-    pictureDescription: "foto de Igor.",
+    pictureDescription() {
+      return `foto de ${this.name}.`;
+    },
+    devSpecialization: "full stack",
   },
   {
-    name: "Carlos",
-    isMale: false,
-    hasBackButton: false,
-    path: "/igor/igor.html",
-    picture:
-      "https://media.licdn.com/dms/image/v2/D4D03AQEQsxrzUhbVkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705305900860?e=1749686400&v=beta&t=rhNEWW401j4BCNHxM5ir0y7l_vE9UC3qnPYY1yIZjdY",
-    pictureDescription: "foto de Igor.",
-  },
-  {
-    name: "Rafael",
+    name: "André",
     isMale: true,
-    hasBackButton: true,
-    path: "/igor/igor.html",
-    picture:
-      "https://media.licdn.com/dms/image/v2/D4D03AQEQsxrzUhbVkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705305900860?e=1749686400&v=beta&t=rhNEWW401j4BCNHxM5ir0y7l_vE9UC3qnPYY1yIZjdY",
-    pictureDescription: "foto de Igor.",
-  },
-  {
-    name: "Maria",
-    isMale: false,
     hasBackButton: false,
-    path: "/igor/igor.html",
-    picture:
-      "https://media.licdn.com/dms/image/v2/D4D03AQEQsxrzUhbVkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705305900860?e=1749686400&v=beta&t=rhNEWW401j4BCNHxM5ir0y7l_vE9UC3qnPYY1yIZjdY",
-    pictureDescription: "foto de Igor.",
+    path: "/igor-000/igor.html",
+    picture: "https://avatars.githubusercontent.com/u/185527961?v=4",
+    pictureDescription() {
+      return `foto de ${this.name}.`;
+    },
+    devSpecialization: "frontend",
   },
 ];
 
-const makeAListOfColaborators = ({
-  name,
-  isMale,
-  path,
-  picture,
-  pictureDescription,
-  hasBackButton,
-}) => {
+const makeAListOfColaborators = (colaborator) => {
   colaboratorsUl.innerHTML += `
     <li class="colaborador">
-    <img class='img-perfil' src="${picture}" alt="${pictureDescription}" />
+    <img class='img-perfil' src="${
+      colaborator.picture
+    }" alt="${colaborator.pictureDescription()}" />
           
             <div class='colaborador-info'>
               
-              <h3>${isMale ? "Colaborador" : "Colaboradora"}</h3>
-              <div>${name}</div>
-              <a href="${path}" target="${
-    hasBackButton ? "_self" : "_blank"
+              <h3 class='colaborador-title'>${
+                colaborator.isMale ? "Colaborador" : "Colaboradora"
+              }</h3>
+              <div class='colaborador-name'>${colaborator.name}</div>
+              <div class='colaborador-area'><b>Área:</b> ${
+                colaborator.devSpecialization
+                  ? colaborator.devSpecialization
+                  : "Não informada"
+              }</div>
+              <a href="${colaborator.path}" class='colaborador-btn' target="${
+    colaborator.hasBackButton ? "_self" : "_blank"
   }">Página pessoal</a>
             </div>
           
@@ -74,4 +63,9 @@ inputEl.addEventListener("keyup", () => {
     name.toLowerCase().includes(inputValue)
   );
   colaboratorsInfoFiltered.forEach(makeAListOfColaborators);
+
+  if (!colaboratorsInfoFiltered.length) {
+    colaboratorsUl.innerHTML =
+      "<li class='not-found'>Colaborador(a) não encontrado(a).</li>";
+  }
 });
