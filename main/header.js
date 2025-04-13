153585af4e48746e3cf4a8b70016f457ec956d9d
Igor Gazio <gaumgazineo@hotmail.com>
2025-04-13 10:55:38 -0300

    trabalhando no layout da descricao

--- main/header.js
+++ main/header.js
@@ -1,6 +1,15 @@
 const menuHamburger = document.getElementById("menu-hamburger");
 const menu = document.getElementById("links-header");
+const links = document.querySelectorAll(".link-header");
+
 menuHamburger.addEventListener("click", () => {
   menuHamburger.classList.toggle("close");
   menu.classList.toggle("expand");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("expand");
+    menuHamburger.classList.toggle("close");
+  });
 });
