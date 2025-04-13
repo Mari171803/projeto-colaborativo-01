c4e647861b9929fbb0bdc2a8b5d14131ec01cc54
Igor Gazio <gaumgazineo@hotmail.com>
2025-04-12 23:41:36 -0300

    atualizando as informações do array colaboratorsInfo

--- main/script.js
+++ main/script.js
@@ -6,47 +6,44 @@ const colaboratorsInfo = [
     name: "Igor Gazineo",
     isMale: true,
     hasBackButton: true,
-    path: "/igor-000/igor.html",
+    path: "../igor-000/igor.html",
     picture:
       "https://media.licdn.com/dms/image/v2/D4D03AQEQsxrzUhbVkg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705305900860?e=1749686400&v=beta&t=rhNEWW401j4BCNHxM5ir0y7l_vE9UC3qnPYY1yIZjdY",
-    pictureDescription() {
-      return `foto de ${this.name}.`;
-    },
     devSpecialization: "full stack",
   },
   {
-    name: "André",
+    name: "Andre Krykhtine",
     isMale: true,
     hasBackButton: false,
-    path: "/igor-000/igor.html",
+    path: "../andreKrykhtine/andre.html",
     picture: "https://avatars.githubusercontent.com/u/185527961?v=4",
-    pictureDescription() {
-      return `foto de ${this.name}.`;
-    },
     devSpecialization: "frontend",
   },
 ];
 
-const makeAListOfColaborators = (colaborator) => {
+const makeAListOfColaborators = ({
+  name,
+  isMale,
+  hasBackButton,
+  path,
+  picture,
+  devSpecialization,
+}) => {
   colaboratorsUl.innerHTML += `
     <li class="colaborador">
-    <img class='img-perfil' src="${
-      colaborator.picture
-    }" alt="${colaborator.pictureDescription()}" />
+    <img class='img-perfil' src="${picture}" alt="Foto de ${name}" />
           
             <div class='colaborador-info'>
               
               <h3 class='colaborador-title'>${
-                colaborator.isMale ? "Colaborador" : "Colaboradora"
+                isMale ? "Colaborador" : "Colaboradora"
               }</h3>
-              <div class='colaborador-name'>${colaborator.name}</div>
+              <div class='colaborador-name'>${name}</div>
               <div class='colaborador-area'><b>Área:</b> ${
-                colaborator.devSpecialization
-                  ? colaborator.devSpecialization
-                  : "Não informada"
+                devSpecialization ? devSpecialization : "Não informada"
               }</div>
-              <a href="${colaborator.path}" class='colaborador-btn' target="${
-    colaborator.hasBackButton ? "_self" : "_blank"
+              <a href="${path}" class='colaborador-btn' target="${
+    hasBackButton ? "_self" : "_blank"
   }">Página pessoal</a>
             </div>
           
